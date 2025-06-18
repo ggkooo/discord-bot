fd207a7d1c5f56fe25f55db5b386e68459a3c341
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-06-18 16:30:16 -0700

    Updating the ModifiedBot class, creating the load_cogs function and adding the persistend ticket view.

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import os
+from cogs.ticket import TicketView
 import discord
 from discord.ext import commands
-from discord import app_commands
 
 class ModifiedBot(commands.Bot):
     def __init__(self, command_prefix, intents):
         super().__init__(command_prefix=command_prefix, intents=intents)
+    
+    async def load_cogs(self):
+        for filename in os.listdir('./cogs'):
+            if filename.endswith('.py'):
+                await self.load_extension(f'cogs.{filename[:-3]}')
 
     async def setup_hook(self):
+        self.add_view(TicketView())
+        await self.load_cogs()
         await self.tree.sync()
 
-bot = ModifiedBot(command_prefix='!', intents=discord.Intents.all())
+bot = ModifiedBot(command_prefix='/', intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
