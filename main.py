e83d0cf20e76d49e4274a2ea186127d91edd1272
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-06-18 17:01:32 -0700

    Adding load_dotenv to the main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from cogs.ticket import TicketView
 import discord
 from discord.ext import commands
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class ModifiedBot(commands.Bot):
     def __init__(self, command_prefix, intents):
         super().__init__(command_prefix=command_prefix, intents=intents)
