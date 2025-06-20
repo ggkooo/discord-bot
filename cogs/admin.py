d6aabc02a0a755fbc5efde8876b8ef59938b2f3f
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-06-20 04:46:27 -0700

    Creating the Admin cog with function '/clear'.

---
+++ cogs/admin.py
@@ -0,0 +1,17 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+class Admin(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        super().__init__()
+
+    @app_commands.command(name='clear', description='A clear command')
+    async def clear(self, interaction: discord.Interaction):
+        await interaction.response.defer(ephemeral=True)
+        deleted = await interaction.channel.purge()
+        await interaction.followup.send(f"{len(deleted)} mensagens foram apagadas.", ephemeral=True)
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Admin(bot))
